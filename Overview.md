c583f507053df8dfbeba452de144dd3940de9edf
Yash Mankar <yash.mankar10122003@gmail.com>
2024-03-31 11:11:23 +0530

    Initial Commit

---
+++ Overview.md
@@ -0,0 +1,12 @@
+## Model
+
+Data Collection
+Data Pre-Processing
+Model Training
+Model Evaluation
+
+## Data Visualization
+
+## Predicitons
+
+## Alerts
--- src/visualization.py
+++ src/visualization.py
@@ -5,6 +5,3 @@ Created on Sun Mar 31 11:01:08 2024
 @author: https://github.com/AraeneaCLI/
 """
 
-print("InfraStat")
-print("V 1.0.0")
-print("Developed By Punarv Patidar & Yash Mankar")
